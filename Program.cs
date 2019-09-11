c5c695c46fc4ba3a0c78bcb4d6cc073313fa69c6
arpit-code1993 <arpitgupta@mail.usf.edu>
2019-09-10 20:56:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years of experience you have");
+             try
+            {   
+             
+                int value_of_input = int.Parse(Console.ReadLine());
+                    
+                    for (int i = 0; i < value_of_input; i++)
+                    {
+                        Console.WriteLine("you have {0} years of experience",value_of_input );
+                    }
+                    Console.WriteLine("Press any key to exit the program ...");
+                    // Pause the program and await the user to press a key to end the program
+                    Console.ReadKey(true);
+                
+            } // End of try
+            catch
+            {
+                Console.WriteLine("Please enter an integer value and try running the program again ...");
+                Console.WriteLine("Press any key to exit the program ...");
+                Console.ReadKey(true);
+            } // End of catch
+        } // End of Main
+    } // End of class
+}
